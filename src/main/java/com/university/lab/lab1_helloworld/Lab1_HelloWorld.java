feb8b4b8223b59e96bd34b617d0c70431a31760c
huzaifa-wk <sehuzaifa24@gmail.com>
2026-08-20 13:03:45 +0500

    Initial Lab 1 commit

---
+++ src/main/java/com/university/lab/lab1_helloworld/Lab1_HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.university.lab.lab1_helloworld;
+
+/**
+ *
+ * @author codez
+ */
+public class Lab1_HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Huzaifa Waheed Khan, Software Construction.");
+        System.out.println("My java environment is successfully configured.");
+    }
+}
